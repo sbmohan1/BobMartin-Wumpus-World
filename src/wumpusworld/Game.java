02e936b6a780e758373aa5e3267b51da69437b06
Damien O'Leary <damienoleary@outlook.com>
2018-02-07 16:06:50 +0000

    Refactor

--- src/wumpusworld/Game.java
+++ src/wumpusworld/Game.java
@@ -5,9 +5,11 @@ import java.awt.Point;
 public class Game {
 	
 	private Map worldMap;
+	private Point wumpusLocation;
 
 	public Game(Map worldMap) {
 		this.worldMap = worldMap;
+		wumpusLocation = new Point(-1, -1);
 	}
 
 	public Point getPlayerPosition() {
@@ -15,8 +17,22 @@ public class Game {
 		do {
 			x = (int)(Math.random() * worldMap.getWidth());
 			y = (int)(Math.random() * worldMap.getHeight());
-		} while(worldMap.isBats(x, y) || worldMap.isPit(x, y) || !worldMap.isCavern(x, y));
+		} while(worldMap.isBats(x, y) || worldMap.isPit(x, y) || !worldMap.isCavern(x, y) || (wumpusLocation.x == x && wumpusLocation.y == y));
 		return new Point(x, y);
+	}
+
+	public void setWumpusLocation(int x, int y) {
+		wumpusLocation = new Point(x, y);
+	}
+
+	public Point getWumpusLocation() {
+		// TODO Auto-generated method stub
+		return wumpusLocation;
+	}
+
+	public Map getWorldMap() {
+		// TODO Auto-generated method stub
+		return worldMap;
 	}
 
 }
