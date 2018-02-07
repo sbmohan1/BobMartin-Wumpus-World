65f733f21d870a0444abc7351cf882e74baa9ea6
Damien O'Leary <damienoleary@outlook.com>
2018-02-07 12:13:25 +0000

    Adding caverns to Map

--- src/wumpusworld/Map.java
+++ src/wumpusworld/Map.java
@@ -1,11 +1,25 @@
 package wumpusworld;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Map {
+	
+	private enum CELL_TYPE {
+		EMPTY,
+		CAVERN
+	}
 	
 	private int width;
 	private int height;
+	private CELL_TYPE[][] grid;
 	
 	public Map(int width, int height) {
+		grid = new CELL_TYPE[width][];
+		for (int i=0; i < width; i++) {
+			grid[i] = new CELL_TYPE[height];
+		}
 		this.width = width;
 		this.height = height;
 	}
@@ -16,6 +30,14 @@ public class Map {
 
 	public int getHeight() {
 		return height;
+	}
+
+	public void addCavern(int x, int y) {
+		grid[x][y] = CELL_TYPE.CAVERN;
+	}
+
+	public boolean isCavern(int x, int y) {
+		return grid[x][y] == CELL_TYPE.CAVERN;
 	}
 
 }
